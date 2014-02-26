546829dcfcaf0b7e978f4c07c13560f611b81a9b
Ferdinand Ribbink <stormtomaz@outlook.com>
2014-02-26 16:43:01 -0500

    refactoring

--- server.js
+++ server.js
@@ -1,51 +1,14 @@
-var express = require('express'),
-    stylus = require('stylus'),
-    mongoose = require('mongoose');
+var express = require('express');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
-function compile(str, path) {
-  return stylus(str).set('filename', path);
-}
+var config = require("./server/config/config")[env];
 
-app.configure(function() {
-  app.set('views', __dirname + '/server/views');
-  app.set('view engine', 'jade');
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(stylus.middleware(
-    {
-      src: __dirname + '/public',
-      compile: compile
-    }
-  ));
-  app.use(express.static(__dirname + '/public'));
-});
+require("./server/config/express")(app, config);
+require("./server/config/mongoose")(config);
+require("./server/config/routes")(app);
 
-if(env === 'development'){
-    mongoose.connect('mongodb://127.3.47.1:27017/multivision');
-}
-else{
-    mongoose.connect('mongodb://usr_ferdy:trusted33@troup.mongohq.com:10024/meandb');
-}
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error...'));
-db.once('open', function callback() {
-  console.log('db opened');
-});
-
-
-app.get('/partials/:partialPath', function(req, res) {
-  res.render('partials/' + req.params.partialPath);
-});
-
-app.get('*', function(req, res) {
-  res.render('index');
-});
-
-var port = Number(process.env.PORT || 5000);
-app.listen(port);
-console.log('Listening on port ' + port + '...');
+app.listen(config.port);
+console.log('Listening on port ' +config.port + '...');
---
+++ server/config/config.js
@@ -0,0 +1,16 @@
+var path = require("path");
+var rootPath = path.normalize(__dirname + "/../../");
+var port = Number(process.env.PORT || 5000);
+
+module.exports={
+    development: {
+        rootPath: rootPath,
+        db: "mongodb://127.3.47.1:27017/multivision",
+        port: port
+    },
+    production: {
+        rootPath: rootPath,
+        db: "mongodb://usr_ferdy:trusted33@troup.mongohq.com:10024/meandb",
+        port: port
+    }
+}
