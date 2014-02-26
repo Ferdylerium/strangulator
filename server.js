651583eecedb70df107ab18aecfbbe31b8e0f7b8
Ferdinand Ribbink <stormtomaz@outlook.com>
2014-02-26 10:11:05 -0500

    woensdag 262

--- server.js
+++ server.js
@@ -31,28 +31,19 @@ else{
     mongoose.connect('mongodb://usr_ferdy:trusted33@troup.mongohq.com:10024/meandb');
 }
 
-//mongoose.connect('mongodb://usr_ferdy:trusted33@troup.mongohq.com:10024/meandb');
-
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error...'));
 db.once('open', function callback() {
-  console.log('db opened ' + db);
+  console.log('db opened');
 });
-var messageSchema = mongoose.Schema({message: String});
-var Message = mongoose.model('Message', messageSchema);
-var mongoMessage;
-Message.findOne().exec(function(err, messageDoc) {
-  mongoMessage = messageDoc.message;
-});
+
 
 app.get('/partials/:partialPath', function(req, res) {
   res.render('partials/' + req.params.partialPath);
 });
 
 app.get('*', function(req, res) {
-  res.render('index', {
-    mongoMessage: mongoMessage
-  });
+  res.render('index');
 });
 
 var port = Number(process.env.PORT || 5000);
